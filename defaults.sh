7c12b71a7fb700fa0e469fd875d77eee4c8d8366
Steve Kuznetsov <steve.kuznetsov@gmail.com>
2015-12-04 01:54:07 -0500

    added sensible flags for 2lev in 2lev, comb, percept

--- defaults.sh
+++ defaults.sh
@@ -3,4 +3,27 @@
 
 source benchmarks.sh
 
-run_benchmarks 'default' 'perfect=-bpred perfect,not-taken=-bpred nottaken,2-bit=-bpred bimod,combined=-bpred comb,two-level=-bpred 2lev'
+run_benchmarks 'default' "perfect=-bpred perfect,not-taken=-bpred nottaken,2-bit=-bpred bimod,\
+combined-1-65536-17-0=-bpred comb -bpred:2lev 1 65536 17 0,\
+combined-1-65536-17-1=-bpred comb -bpred:2lev 1 65536 17 1,\
+combined-1-65536-12-0=-bpred comb -bpred:2lev 1 65536 12 0,\
+combined-64-65536-17-0=-bpred comb -bpred:2lev 64 65536 17 0,\
+combined-64-65536-12-0=-bpred comb -bpred:2lev 64 65536 12 0,\
+combined-1024-65536-17-0=-bpred comb -bpred:2lev 1024 65536 17 0,\
+combined-1024-65536-12-0=-bpred comb -bpred:2lev 1024 65536 12 0,\
+two-level-1-65536-17-0=-bpred 2lev -bpred:2lev 1 65536 17 0,\
+two-level-1-65536-17-1=-bpred 2lev -bpred:2lev 1 65536 17 1,\
+two-level-1-65536-12-0=-bpred 2lev -bpred:2lev 1 65536 12 0,\
+two-level-64-65536-17-0=-bpred 2lev -bpred:2lev 64 65536 17 0,\
+two-level-64-65536-12-0=-bpred 2lev -bpred:2lev 64 65536 12 0,\
+two-level-1024-65536-17-0=-bpred 2lev -bpred:2lev 1024 65536 17 0,\
+two-level-1024-65536-12-0=-bpred 2lev -bpred:2lev 1024 65536 12 0"
+
+# we want the size of the simulated predictor that we are implementing to be roughly 64KB
+# order is always
+# gag
+# gas
+# sag
+# sas
+# pag
+# pas
--- experimental.sh
+++ experimental.sh
@@ -3,4 +3,19 @@
 
 source benchmarks.sh
 
-run_benchmarks 'experimental' 'percept=-bpred percept' #'-bpred oghel'
+run_benchmarks 'experimental' "\
+percept-1-2048-32=-bpred percept -bpred:2lev 1 2048 32 0,\
+percept-1-1024-64=-bpred percept -bpred:2lev 1 1024 64 0,\
+percept-1-512-128=-bpred percept -bpred:2lev 1 512 128 0,\
+percept-64-2048-32=-bpred percept -bpred:2lev 64 2048 32 0,\
+percept-64-1024-64=-bpred percept -bpred:2lev 64 1024 64 0,\
+percept-64-512-128=-bpred percept -bpred:2lev 64 512 128 0,\
+percept-1024-1024-32=-bpred percept -bpred:2lev 1024 1024 32 0,\
+percept-1024-512-64=-bpred percept -bpred:2lev 1024 512 64 0,\
+percept-1024-256-128=-bpred percept -bpred:2lev 1024 256 128 0"
+
+# we want the size of the simulated predictor that we are implementing to be roughly 64KB
+# order is always
+# gagx3
+# sagx3
+# pagx3
